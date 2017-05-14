99d969f31959d3fef98e171f88c9e2e8f501fb47
luabeltranga <luabeltranga@unal.edu.co>
2017-05-14 13:14:58 -0500

    se agregan comentarios al programa

--- votaciones1.cpp
+++ votaciones1.cpp
@@ -1,3 +1,8 @@
+//para crear el gif descomentar start_gnuplot y print gnuplot, comentar en stop
+//los cout y dentro del buqle de repeticiones el cout
+
+
+//compilar con make vot1 en consola
 #include <iostream>
 #include <random>
 #include <vector>
@@ -11,14 +16,22 @@ void clean_lattice(std::vector<int> &latt);
 bool stop(std::vector<int> &latt);
 
 int main (void){
+  //tamaño de la malla
   const int N = 10;
-  const int repetitions = 100000; 
+
+  //numero de pasos maximos para alcanzar el consenso
+  const int max = 100000; 
+
+  //numero de repeticiones
+  const int rep = 20;
+  //creacion de la malla como vector unidimensional
   std::vector<int> latt (N*N);
   //start_gnuplot();
-  //random values 0 and 1 on the lattice 
-  for(int jj = 0; jj < 1; jj++){
+
+  for(int jj = 0; jj < rep; jj++){
+    
     initialize_lattice(latt);
-    for (int ii = 0 ; ii < repetitions ; ii++){
+    for (int ii = 0 ; ii < max ; ii++){
       // print_gnuplot(latt);
       interaction(latt);
       if(stop(latt) == true){
@@ -32,9 +45,17 @@ int main (void){
   return 0;
 }
 
+//llena de forma aleatoria la malla de 0 y 1
 void initialize_lattice (std::vector<int> &latt){
+  //generador de semilla aleatoria
   std::random_device rd;
+
+  //generador de numeros aleatorios para el llenado de la malla
   std::mt19937 gen(10);
+
+  //funcion que toma el generador de numeros aleatorios para la
+  //generacion de numeros entre 0 y 1 usando una distribucion uniforme
+  //los cuales se asignan a la malla
   std::uniform_int_distribution<int> dis(0,1);
   int ii = 0;
   int jj = 0;
@@ -46,6 +67,7 @@ void initialize_lattice (std::vector<int> &latt){
     }  
 }
 
+//limpia la malla para correr de nuevo 
 void clean_lattice(std::vector<int> &latt){
   int N = std::sqrt(latt.size());
     for(int ii = 0 ; ii < N ; ii++){
@@ -55,6 +77,7 @@ void clean_lattice(std::vector<int> &latt){
     }  
 }
 
+//imprime la malla en gnuplot(opcional)
 void print_gnuplot(std::vector<int> &latt){
   const int N = std::sqrt(latt.size());
   std::cout<<"plot [-0.5:"<< N-0.5 <<"] [-0.5:"<< N-0.5 <<"] '-'  matrix with image "<< std::endl;
@@ -68,7 +91,7 @@ void print_gnuplot(std::vector<int> &latt){
   std::cout << "e " << std::endl;
   std::cout << "e " << std::endl;
 }
-
+//inicia gnuplot con opciones de gif (opcional)
 void start_gnuplot(void){
   std::cout << "unset key" << std::endl;
   std::cout << "set palette model RGB" << std::endl;
@@ -79,29 +102,40 @@ void start_gnuplot(void){
   std::cout << std::endl;
 }
 
+
+
+//inicia el intercambio
 void interaction(std::vector<int> &latt){
+  
   int N = std::sqrt(latt.size());
+  //generador de la semilla para el generador de numeros aleatorios
   std::random_device rd;
+
+  //generador con semilla activa
   std::mt19937_64 gen(rd());
-  //for the election of the voter
+  
+  //se utiliza una distribucion uniforme para elegir un elemento de la malla
   std::uniform_int_distribution<int> dis1(0,N-1);
   
-  //for the interaction between elements without the border
+  //para la eleccion de los vecinos de elementos fuera de la frontera
   std::uniform_int_distribution<int> dis2(1,8);
   
-  //for the interaction on the border
+  //para la eleccion de los vecinos sobre la frontera sin esquinas
   std::uniform_int_distribution<int> dis3(1,5);
   
-  //for the intetaction on the tips of the lattice
+  //para la eleccion de los vecinos en las esquinas
   std::uniform_int_distribution<int> dis4(1,3);
-  
+
+  //genera coordenadas en la malla
   int ii = dis1(gen);
   int jj = dis1(gen);
+
+  //pruebas para eleccion de vecinos
   int test1 = dis2(gen);
   int test2 = dis3(gen);
   int test3 = dis4(gen);
   
-  //borde superior
+  //implementacion de interaccion en borde superior 
   if(ii == 0 && (jj>0 && jj < N-1)){
 
     if(test2 == 1){
@@ -120,7 +154,7 @@ void interaction(std::vector<int> &latt){
       latt[ii*N+jj] = latt[ii*N+(jj+1)];    
     }
   }
-  //borde inferior
+  //implementacion de interaccion en borde inferior
   else if(ii == N-1 && (jj>0 && jj < N-1)){
     if(test2 == 1){
       latt[ii*N+jj] = latt[ii*N+(jj-1)];    
@@ -138,7 +172,7 @@ void interaction(std::vector<int> &latt){
       latt[ii*N+jj] = latt[ii*N+(jj+1)];    
     }
   }
-  //borde izquierdo
+  //implementacion de interaccion en borde izquierdo
   else if(jj == 0 && (ii>0 && ii < N-1)){
     if(test2 == 1){
       latt[ii*N+jj] = latt[(ii-1)*N+jj];    
@@ -156,7 +190,7 @@ void interaction(std::vector<int> &latt){
       latt[ii*N+jj] = latt[(ii+1)*N+jj];    
     }
   }
-  //borde derecho
+  //implementacion de interaccion en borde derecho
   else if(jj == N-1 && (ii>0 && ii < N-1)){
     if(test2 == 1){
       latt[ii*N+jj] = latt[(ii-1)*N+jj];    
@@ -175,7 +209,7 @@ void interaction(std::vector<int> &latt){
     }
   }
 
-  //the tips of the lattice
+  //implementacion de interaccion en las esquinas de la malla
   else if(ii == 0 && jj == 0){
     if(test3 == 1){
       latt[ii*N+jj] = latt[ii*N+(jj+1)];    
@@ -221,7 +255,9 @@ void interaction(std::vector<int> &latt){
       latt[ii*N+jj] = latt[(ii-1)*N+jj];    
     }
   }
-  //the rest of the lattice
+
+  
+  //implementacion de interaccion en el interior de la malla
   else{
     if(test1 == 1){
       latt[ii*N+jj] = latt[ii*N+(jj+1)];    
@@ -250,6 +286,8 @@ void interaction(std::vector<int> &latt){
   }
 }
 
+
+//criterio de parada cuando se alcanza el consenso 
 bool stop(std::vector<int> &latt){
   int N = std::sqrt(latt.size());
   unsigned long sum = 0;
@@ -263,7 +301,7 @@ bool stop(std::vector<int> &latt){
     return true;
   }
   if(sum == 0){
-    std::cout << "win 0" << std::endl;
+    std::cout << "win -1" << std::endl;
     return true;
   }
   return false;
